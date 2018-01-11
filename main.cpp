358792eb3fc0ed8827d65d69dadb0be59aba64e4
Wataru Furusho <knowledge.kudryavka@gmail.com>
2018-01-11 17:50:48 +0900

    Runnable Granule cells.

--- main.cpp
+++ main.cpp
@@ -8,9 +8,8 @@
 #include <mpi.h>
 
 #define MAX_DEVICE_IDS (8)
-#define WORK_UNIT_SIZE (15872) //1984PE * 8Thread
 
-//#include "param.h"
+#include "param.h"
 #define MAX_BINARY_SIZE (0x1000000)
 #define KERNEL_FILE "./kernel.sc2/kernel.sc2.pz"
 #define KERNEL_FUNC "kernel"
@@ -18,7 +17,7 @@
 void Error(const char* message, ...) {
   va_list ap;
   va_start(ap, message);
-  fprintf(stderr, "Error:");
+  fprintf(stderr, "Error : ");
   vfprintf(stderr, message, ap);
   va_end(ap);
   exit(1);
@@ -87,47 +86,113 @@ int main (int argc, char *argv[]) {
   pzcl_command_queue queue = pzclCreateCommandQueue(context, device[device_id], 0, &err);
   if (err) Error("pzclCreateCommandQueue: %d\n", err);
 
-  // Set data
-  int n = 10;
-  float* a = (float*)malloc(sizeof(float) * n);
-  for (int i = 0; i < n; i++) a[i] = 1.0;
-  pzcl_mem dev_a = pzclCreateBuffer(context, PZCL_MEM_READ_WRITE, sizeof(float) * n, NULL, &err);
-  if (err) Error("pzclCreateBuffer: %d\n", err);
-  err = pzclEnqueueWriteBuffer(queue, dev_a, PZCL_TRUE, 0, sizeof(float) * n, a, 0, NULL, NULL);
-  if (err) Error("pzclEnqueueWriteBuffer: %d\n", err);
-  float* b = (float*)malloc(sizeof(float) * n);
-  for (int i = 0; i < n; i++) b[i] = 1.0;
-  pzcl_mem dev_b = pzclCreateBuffer(context, PZCL_MEM_READ_WRITE, sizeof(float) * n, NULL, &err);
-  if (err) Error("pzclCreateBuffer %d\n", err);
-  err = pzclEnqueueWriteBuffer(queue, dev_b, PZCL_TRUE, 0, sizeof(float) * n, b, 0, NULL, NULL);
-  if (err) Error("pzclEnqueueWriteBuffer: %d\n", err);
-  float res;
-  pzcl_mem dev_res = pzclCreateBuffer(context, PZCL_MEM_READ_WRITE, sizeof(float), NULL, &err);
-  if (err) Error("pzclCreateBuffer %d\n", err);
+  //
+  // Data Setup
+  //
 
-  pzclSetKernelArg(kernel, 0, sizeof(int), &n);
-  pzclSetKernelArg(kernel, 1, sizeof(pzcl_mem), &dev_a);
-  pzclSetKernelArg(kernel, 2, sizeof(pzcl_mem), &dev_b);
-  pzclSetKernelArg(kernel, 3, sizeof(pzcl_mem), &dev_res);
+  unsigned long* s_gr = (unsigned long*)malloc(sizeof(unsigned long) * N_S_GR);
+  if (s_gr == NULL) Error("failed malloc s_gr\n");
+  for (int i = 0; i < N_S_GR; i++) s_gr[i] = 0;
+  pzcl_mem dev_s_gr = pzclCreateBuffer(context, PZCL_MEM_READ_WRITE, sizeof(unsigned long) * N_S_GR, NULL, &err);
+  if (err) Error("pzclCreateBuffer:%d dev_s_gr\n", err);
+  err = pzclEnqueueWriteBuffer(queue, dev_s_gr, PZCL_TRUE, 0, sizeof(unsigned long) * N_S_GR, s_gr, 0, NULL, NULL);
+  if (err) Error("pzclEnqueueWriteBuffer:%d s_gr\n", err);
+
+  float* u = (float*)malloc(sizeof(float) * N_ALL);
+  if (u == NULL) Error("failed malloc u\n");
+  for (int i = 0; i < N_ALL; i++) {
+    float e_leak = ( GR(i) * (E_LEAK_GR) );
+    u[i] = e_leak;
+  }
+  pzcl_mem dev_u = pzclCreateBuffer(context, PZCL_MEM_READ_WRITE, sizeof(float) * N_ALL, NULL, &err);
+  if (err) Error("pzclCreateBuffer:%d dev_u\n", err);
+  err = pzclEnqueueWriteBuffer(queue, dev_u, PZCL_TRUE, 0, sizeof(float) * N_ALL, u, 0, NULL, NULL);
+  if (err) Error("pzclEnqueueWriteBuffer:%d u\n", err);
+
+  float* g_ahp = (float*)malloc(sizeof(float) * N_ALL);
+  if (g_ahp == NULL) Error("failed malloc g_ahp\n");
+  for (int i = 0; i < N_ALL; i++) g_ahp[i] = 0.0f;
+  pzcl_mem dev_g_ahp = pzclCreateBuffer(context, PZCL_MEM_READ_WRITE, sizeof(float) * N_ALL, NULL, &err);
+  if (err) Error("pzclCreateBuffer:%d dev_g_ahp\n", err);
+  err = pzclEnqueueWriteBuffer(queue, dev_g_ahp, PZCL_TRUE, 0, sizeof(float) * N_ALL, g_ahp, 0, NULL, NULL);
+  if (err) Error("pzclEnqueueWriteBuffer:%d g_ahp\n", err);
+
+  char* spikep_buf = (char*)malloc(sizeof(char) * WORK_UNIT_SIZE * T_I);
+  if (spikep_buf == NULL) Error("failed malloc spikep_buf\n");
+  for (int i = 0; i < WORK_UNIT_SIZE * T_I; i++) spikep_buf[i] = 0;
+  pzcl_mem dev_spikep_buf = pzclCreateBuffer(context, PZCL_MEM_READ_WRITE, sizeof(char) * WORK_UNIT_SIZE * T_I, NULL, &err);
+  if (err) Error("pzclCreateBuffer:%d dev_spikep_buf\n", err);
+  err = pzclEnqueueWriteBuffer(queue, dev_spikep_buf, PZCL_TRUE, 0, sizeof(char) * WORK_UNIT_SIZE * T_I, spikep_buf, 0, NULL, NULL);
+  if (err) Error("pzclEnqueueWriteBuffer:%d spikep_buf\n", err);
+
+  char* h_spikep_buf = (char*)malloc(sizeof(char) * WORK_UNIT_SIZE * N_PERIOD);
 
   size_t work_unit_size = WORK_UNIT_SIZE;
   pzcl_event event = NULL;
-  err = pzclEnqueueNDRangeKernel(queue, kernel, 1, NULL, &work_unit_size, NULL, 0, NULL, &event);
-  if (err) Error("pzclEnqueueNDRangeKernel: %d\n", err);
-  if (event) {
-    err = pzclWaitForEvents(1, &event);
-    if (err) Error("pzclWaitForEvents: %d in trial.\n", err);
+
+  for (int nt = 0; nt < N_TRIALS; nt++) {
+    FILE* fgr_spk;
+    if (world_rank == 0) {
+      char fn[1024];
+      sprintf(fn, "gr.spk.%d.%d", world_rank, nt);
+      fgr_spk = fopen(fn, "w");
+      fprintf(stderr, "== %2d ==\n", nt);
+    }
+
+    pzclSetKernelArg(kernel, 0, sizeof(pzcl_mem), &dev_s_gr);
+    pzclSetKernelArg(kernel, 1, sizeof(pzcl_mem), &dev_u);
+    pzclSetKernelArg(kernel, 2, sizeof(pzcl_mem), &dev_g_ahp);
+    pzclSetKernelArg(kernel, 3, sizeof(pzcl_mem), &dev_spikep_buf);
+
+    if (world_rank == 0) timer_start();
+
+    int count = 0;
+    for (int t_e = 0; t_e < N_PERIOD; t_e += T_I) {
+      count++;
+
+      err = pzclEnqueueNDRangeKernel(queue, kernel, 1, NULL, &work_unit_size, NULL, 0, NULL, &event);
+      if (err) Error("pzclEnqueueNDRangeKernel: %d\n", err);
+      if (event) {
+        err = pzclWaitForEvents(1, &event);
+        if (err) Error("pzclWaitForEvents: %d in trial.\n", err);
+      }
+
+      err = pzclEnqueueReadBuffer(queue, dev_spikep_buf, PZCL_TRUE, 0, sizeof(char) * WORK_UNIT_SIZE * T_I, spikep_buf, 0, NULL, NULL);
+      if (err) Error("pzclEnqueueReadBuffer: %d\n", err);
+      if (spikep_buf == NULL) Error("Enpty spikep_buf.\n");
+
+      for (int t_i = 0; t_i < T_I; t_i++) {
+        int t_each = t_e + t_i;
+        for (int i = 0; i < WORK_UNIT_SIZE; i++) {
+          h_spikep_buf[WORK_UNIT_SIZE * t_each + i] = spikep_buf[WORK_UNIT_SIZE * t_i + i];
+        }
+      }
+    }
+
+    if (world_rank == 0) {
+      double elapsed_time = timer_elapsed();
+      fprintf(stderr, "time: %f msec\n", elapsed_time);
+
+      for (int t = 0; t < N_PERIOD; t++) {
+        for (int i = 0; i < WORK_UNIT_SIZE; i++)
+          if (h_spikep_buf[t * WORK_UNIT_SIZE + i] != 0) fprintf(fgr_spk, "%d %d\n", t, i);
+      }
+      fclose(fgr_spk);
+    }
   }
-  err = pzclEnqueueReadBuffer(queue, dev_res, PZCL_TRUE, 0, sizeof(float), &res, 0, NULL, NULL);
-  if (err) Error("pzclEnqueueReadBuffer: %d\n", err);
-  printf("%f\n", res);
-  free(a);
-  free(b);
+
+  // Release memory objects
+  free(s_gr);
+  free(u);
+  free(g_ahp);
+  free(spikep_buf);
 
   // Release PZCL Resources
-  pzclReleaseMemObject(dev_a);
-  pzclReleaseMemObject(dev_b);
-  pzclReleaseMemObject(dev_res);
+  pzclReleaseEvent(event);
+  pzclReleaseMemObject(dev_s_gr);
+  pzclReleaseMemObject(dev_u);
+  pzclReleaseMemObject(dev_g_ahp);
+  pzclReleaseMemObject(dev_spikep_buf);
   pzclReleaseCommandQueue(queue);
   pzclReleaseKernel(kernel);
   pzclReleaseProgram(program);
