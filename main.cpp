72118574646d9b4d77b4589c684d0c4e572824d6
Wataru Furusho <knowledge.kudryavka@gmail.com>
2018-01-17 00:22:58 +0900

    Connect GR -> GO

--- main.cpp
+++ main.cpp
@@ -34,6 +34,107 @@ double timer_elapsed(void) {
   return (elapsed.tv_sec * 1000.0 + elapsed.tv_usec / 1000.0);
 }
 
+int compare_unsigned_short(const void* a, const void* b) {
+  return *(unsigned short*)a - *(unsigned short*)b;
+}
+void setup_grlist(unsigned short* list_gogr, unsigned short* list_grgo) {
+  int gox, goy;
+  int goax, goay, godx, gody;
+  int gon, goan;
+  int nlist_gogr[N_GO], nlist_grgo[N_GO];
+
+  for (int i = 0; i < N_GO; i++) nlist_gogr[i] = 0;
+  for (int i = 0; i < N_GO; i++) nlist_grgo[i] = 0;
+
+  for (goy = 0; goy < Y_GO; goy++) {
+    for (gox = 0; gox < X_GO; gox++) {
+      gon = goy * X_GO + gox;
+      for (int i = 0; i < N_GR_PER_GO; i++) {
+        godx = -3 + (int)(7.0f * (float)rand() / (float)RAND_MAX);
+        gody = -3 + (int)(7.0f * (float)rand() / (float)RAND_MAX);
+        goax = gox + godx + 3 * X_GO / 4;
+        if (goax >= X_GO) goax -= X_GO;
+        if (goax < 0) goax += X_GO;
+        goay = goy + gody + Y_GO / 2;
+        if (goay >= Y_GO) goay -= Y_GO;
+        if (goay < 0) goay += Y_GO;
+        goan = goay * X_GO + goax;
+        list_gogr[gon * N_GO + i] = goan;
+        nlist_gogr[gon]++;
+      }
+    }
+  }
+
+  int** arr;
+  int naxons[N_GO];
+  arr = (int**)malloc(sizeof(int*) * N_GO);
+  for (int i = 0; i < N_GO; i++) arr[i] = (int*)malloc(sizeof(int) * N_GO);
+  for (int i = 0; i < N_GO; i++) naxons[i] = 0;
+
+  for (goy = 0; goy < Y_GO; goy++) {
+    for (gox = 0; gox < X_GO; gox++) {
+      gon = goy * X_GO + gox;
+      for (int i = 0; i < N_GO_PER_GR / 4; i++) {
+        godx = -4 + (int)(9.0f * (float)rand() / (float)RAND_MAX);
+        gody = -4 + (int)(9.0f * (float)rand() / (float)RAND_MAX);
+        goax = gox + godx;
+        if (goax >= X_GO) goax -= X_GO;
+        if (goax < 0) goax += X_GO;
+        goay = goy + gody;
+        if (goay >= Y_GO) goay -= Y_GO;
+        if (goay < 0) goay += Y_GO;
+        goan = goay * X_GO + goax;
+        arr[gon][naxons[gon]] = goan;
+        naxons[gon]++;
+      }
+    }
+  }
+  int grn;
+  for (gox = 0; gox < X_GO; gox++) {
+    for (goy = 0; goy < Y_GO; goy++) {
+      gon = goy * X_GO + gox;
+      grn = gon;
+      for (godx = 0; godx <= 1; godx++) {
+        goax = gox + godx;
+        if (goax >= X_GO) goax -= X_GO;
+        for (gody = 0; gody <= 1; gody++) {
+          goay = goy + gody;
+          if (goay >= Y_GO) goay -= Y_GO;
+          goan = goay * X_GO + goax;
+          for (int i = 0; i < naxons[goan]; i++) {
+            list_grgo[grn * N_GO + nlist_grgo[grn]] = arr[goan][i];
+            nlist_grgo[grn]++;
+          }
+        }
+      }
+    }
+  }
+  for (int i = 0; i < N_GO; i++) free(arr[i]);
+  free(arr);
+
+  // Check the number of afferent inputs
+  for (int i = 0; i < N_GO; i++) {
+    if (nlist_grgo[i] != N_GO_PER_GR) Error("nlist_grgo[%d] = %d, N_GO_PER_GR = %d\n", i, nlist_grgo[i], N_GO_PER_GR);
+    if (nlist_gogr[i] != N_GR_PER_GO) Error("nlist_gogr[%d] = %d, N_GR_PER_GO = %d\n", i, nlist_gogr[i], N_GR_PER_GO);
+  }
+
+  // Sort list to make accessing easy.
+  for (int i = 0; i < N_GO; i++) {
+    qsort(&list_grgo[N_GO * i], N_GO_PER_GR, sizeof(unsigned short), compare_unsigned_short);
+    qsort(&list_gogr[N_GO * i], N_GR_PER_GO, sizeof(unsigned short), compare_unsigned_short);
+  }
+
+  // Check the number of lists.
+  for (int i = 0; i < N_GO; i++) {
+    for (int j = 0; j < N_GO_PER_GR; j++)
+      if (list_grgo[i * N_GO + j] < 0 || list_grgo[i * N_GO + j] >= N_GO)
+        Error("Failed to make list_grgo\n");
+    for (int j = 0; j < N_GR_PER_GO; j++)
+      if (list_gogr[i * N_GO + j] < 0 || list_gogr[i * N_GO + j] >= N_GO)
+        Error("Failed to make list_gogr\n");
+  }
+}
+
 int main (int argc, char *argv[]) {
   // MPI Setup
   MPI_Init(NULL, NULL);
@@ -98,6 +199,14 @@ int main (int argc, char *argv[]) {
   err = pzclEnqueueWriteBuffer(queue, dev_s_gr, PZCL_TRUE, 0, sizeof(unsigned long) * N_S_GR, s_gr, 0, NULL, NULL);
   if (err) Error("pzclEnqueueWriteBuffer:%d s_gr\n", err);
 
+  unsigned char* s_go = (unsigned char*)malloc(sizeof(unsigned char) * N_GO);
+  if (s_go == NULL) Error("failed malloc s_go\n");
+  for (int i = 0; i < N_GO; i++) s_go[i] = 0;
+  pzcl_mem dev_s_go = pzclCreateBuffer(context, PZCL_MEM_READ_WRITE, sizeof(unsigned char) * N_GO, NULL, &err);
+  if (err) Error("pzclCreateBuffer:%d dev_s_go\n", err);
+  err = pzclEnqueueWriteBuffer(queue, dev_s_go, PZCL_TRUE, 0, sizeof(unsigned char) * N_GO, s_go, 0, NULL, NULL);
+  if (err) Error("pzclEnqueueWriteBuffer:%d s_go\n", err);
+
   srand(23L);
   unsigned int* seeds = (unsigned int*)malloc(sizeof(unsigned int) * N_GR);
   if (seeds == NULL) Error("failed malloc seeds\n");
@@ -107,10 +216,21 @@ int main (int argc, char *argv[]) {
   err = pzclEnqueueWriteBuffer(queue, dev_seeds, PZCL_TRUE, 0, sizeof(unsigned int) * N_GR, seeds, 0, NULL, NULL);
   if (err) Error("pzclEnqueueWriteBuffer:%d seeds\n", err);
 
+  unsigned short* lists = (unsigned short*)malloc(sizeof(unsigned short) * N_GO * N_GO * 2);
+  if (lists == NULL) Error("Failed malloc lists\n");
+  unsigned short* list_gogr = lists;
+  unsigned short* list_grgo = lists + N_GO * N_GO;
+  setup_grlist(list_gogr, list_grgo);
+  pzcl_mem dev_lists = pzclCreateBuffer(context, PZCL_MEM_READ_WRITE, sizeof(unsigned short) * N_GO * N_GO * 2, NULL, &err);
+  if (err) Error("pzclCreateBuffer:%d dev_lists\n", err);
+  err = pzclEnqueueWriteBuffer(queue, dev_lists, PZCL_TRUE, 0, sizeof(unsigned short) * N_GO * N_GO * 2, lists, 0, NULL, NULL);
+  if (err) Error("pzclEnqueueWriteBuffer:%d lists\n", err);
+
   float* u = (float*)malloc(sizeof(float) * N_ALL);
   if (u == NULL) Error("failed malloc u\n");
   for (int i = 0; i < N_ALL; i++) {
-    float e_leak = ( GR(i) * (E_LEAK_GR) );
+    float e_leak = ( GR(i) * (E_LEAK_GR) 
+                    +GO(i) * (E_LEAK_GO));
     u[i] = e_leak;
   }
   pzcl_mem dev_u = pzclCreateBuffer(context, PZCL_MEM_READ_WRITE, sizeof(float) * N_ALL, NULL, &err);
@@ -149,19 +269,24 @@ int main (int argc, char *argv[]) {
 
   for (int nt = 0; nt < N_TRIALS; nt++) {
     FILE* fgr_spk;
+    FILE* log;
     if (world_rank == 0) {
       char fn[1024];
       sprintf(fn, "gr.spk.%d.%d", world_rank, nt);
       fgr_spk = fopen(fn, "w");
+      sprintf(fn, "log.%d.%d", world_rank, nt);
+      log = fopen(fn, "w");
       fprintf(stderr, "== %2d ==\n", nt);
     }
 
     pzclSetKernelArg(kernel, 0, sizeof(pzcl_mem), &dev_seeds);
-    pzclSetKernelArg(kernel, 1, sizeof(pzcl_mem), &dev_s_gr);
-    pzclSetKernelArg(kernel, 2, sizeof(pzcl_mem), &dev_u);
-    pzclSetKernelArg(kernel, 3, sizeof(pzcl_mem), &dev_g_ex);
-    pzclSetKernelArg(kernel, 4, sizeof(pzcl_mem), &dev_g_ahp);
-    pzclSetKernelArg(kernel, 5, sizeof(pzcl_mem), &dev_spikep_buf);
+    pzclSetKernelArg(kernel, 1, sizeof(pzcl_mem), &dev_lists);
+    pzclSetKernelArg(kernel, 2, sizeof(pzcl_mem), &dev_s_gr);
+    pzclSetKernelArg(kernel, 3, sizeof(pzcl_mem), &dev_s_go);
+    pzclSetKernelArg(kernel, 4, sizeof(pzcl_mem), &dev_u);
+    pzclSetKernelArg(kernel, 5, sizeof(pzcl_mem), &dev_g_ex);
+    pzclSetKernelArg(kernel, 6, sizeof(pzcl_mem), &dev_g_ahp);
+    pzclSetKernelArg(kernel, 7, sizeof(pzcl_mem), &dev_spikep_buf);
 
     if (world_rank == 0) timer_start();
 
@@ -169,7 +294,7 @@ int main (int argc, char *argv[]) {
     for (int t_e = 0; t_e < N_PERIOD; t_e += T_I) {
       count++;
 
-      pzclSetKernelArg(kernel, 6, sizeof(int), &t_e);
+      pzclSetKernelArg(kernel, 8, sizeof(int), &t_e);
 
       err = pzclEnqueueNDRangeKernel(queue, kernel, 1, NULL, &work_unit_size, NULL, 0, NULL, &event);
       if (err) Error("pzclEnqueueNDRangeKernel: %d\n", err);
@@ -181,6 +306,14 @@ int main (int argc, char *argv[]) {
       err = pzclEnqueueReadBuffer(queue, dev_spikep_buf, PZCL_TRUE, 0, sizeof(char) * N_ALL_P * T_I, spikep_buf, 0, NULL, NULL);
       if (err) Error("pzclEnqueueReadBuffer: %d\n", err);
       if (spikep_buf == NULL) Error("Enpty spikep_buf.\n");
+#ifdef DEBUG
+      err = pzclEnqueueReadBuffer(queue, dev_u, PZCL_TRUE, 0, sizeof(float) * N_ALL, u, 0, NULL, NULL);
+      if (err) Error("pzclEnqueueReadBuffer: %d\n", err);
+      fprintf(log, "v:%f\n", u[N_GR]);
+      err = pzclEnqueueReadBuffer(queue, dev_g_ex, PZCL_TRUE, 0, sizeof(float) * N_ALL, g_ex, 0, NULL, NULL);
+      if (err) Error("pzclEnqueueReadBuffer: %d\n", err);
+      fprintf(log, "g_ex:%f\n", g_ex[N_GR]);
+#endif
 
       for (int t_i = 0; t_i < T_I; t_i++) {
         int t_each = t_e + t_i;
@@ -199,19 +332,28 @@ int main (int argc, char *argv[]) {
           if (h_spikep_buf[t * N_ALL_P + i] != 0) fprintf(fgr_spk, "%d %d\n", t, i);
       }
       fclose(fgr_spk);
+      fclose(log);
     }
   }
 
   // Release memory objects
+  free(seeds);
+  free(lists);
   free(s_gr);
+  free(s_go);
   free(u);
+  free(g_ex);
   free(g_ahp);
   free(spikep_buf);
 
   // Release PZCL Resources
   pzclReleaseEvent(event);
+  pzclReleaseMemObject(dev_seeds);
+  pzclReleaseMemObject(dev_lists);
   pzclReleaseMemObject(dev_s_gr);
+  pzclReleaseMemObject(dev_s_go);
   pzclReleaseMemObject(dev_u);
+  pzclReleaseMemObject(dev_g_ex);
   pzclReleaseMemObject(dev_g_ahp);
   pzclReleaseMemObject(dev_spikep_buf);
   pzclReleaseCommandQueue(queue);
--- param.h
+++ param.h
@@ -1,8 +1,11 @@
 #define R_N2 (32 * 32)
 
 #define N_GR (32 * 32 * R_N2)
+#define X_GO (32)
+#define Y_GO (32)
+#define N_GO (1024) // X_GO * Y_GO
 
-#define N_ALL ((N_GR))
+#define N_ALL ((N_GR) + (N_GO))
 
 #define N_S_GR (N_GR / 64)
 
@@ -26,11 +29,41 @@
 #define R_AMPA_GRMF     (0.88f)
 #define R_NMDA_GRMF     (0.12f)
 #define LAMBDA          (16.0f)
+#define N_GO_PER_GR     (8)
+
+#define TH_GO        (-52.0f)
+#define C_GO         (28.0f)
+#define INV_C_GO     (0.035714f)
+#define R_GO         (0.428f)
+#define GBAR_LEAK_GO (2.3f) //2.3nS
+#define E_LEAK_GO    (-55.0f)
+#define GBAR_EX_GO   (45.5f)
+#define E_EX_GO      (0.0f)
+#define GBAR_INH_GO  (0.0f) // No inhibitory inputs
+#define E_INH_GO     (E_LEAK_GO)
+#define GBAR_AHP_GO  (20.0f) //4.2nS
+#define DECAY_AHP_GO (0.81873f) //exp(-(float)DT/tau_ahp_go);
+#define E_AHP_GO     (-72.7f)
+#define I_EX_GO      (0.0f)
+
+#define N_GR_PER_GO      (4)
+#define DECAY_AMPA_GOGR  (0.51342f) //exp(-(float)DT/tau_ampa_gogr);
+#define DECAY_AMPA_GOGR2 (0.26360f) //DECAY_AMPA_GOGR*DECAY_AMPA_GOGR
+#define DECAY_AMPA_GOGR3 (0.13534f) //DECAY_AMPA_GOGR2*DECAY_AMPA_GOGR
+#define DECAY_NMDA1_GOGR (0.96826f) //exp(-(float)DT/tau1_nmda_gogr);
+#define DECAY_NMDA2_GOGR (0.99413f) //exp(-(float)DT/tau2_nmda_gogr);
+#define R_AMPA_GOGR      (0.8f)
+#define R_NMDA1_GOGR     (0.066f)   //(0.2*0.33)
+#define R_NMDA2_GOGR     (0.134f)   //(0.2*0.67)
+#define KAPPA_GOGR       (4.0e-5f)
 
 #define IDX_H_GR (0)
 #define IDX_T_GR ((IDX_H_GR) + (N_GR))
+#define IDX_H_GO (IDX_T_GR)
+#define IDX_T_GO ((IDX_H_GO) + (N_GO))
 
 #define GR(i) (((IDX_H_GR) <= (i)) && ((i) < (IDX_T_GR)))
+#define GO(i) (((IDX_H_GO) <= (i)) && ((i) < (IDX_T_GO)))
 
 #define N_TRIALS (1)
 #define N_PERIOD (6000) // 6000 msec
@@ -42,4 +75,8 @@
 #define N_MAX_THREADS (WORK_UNIT_SIZE)
 
 #define N_GR_P (128)
-#define N_ALL_P ((N_GR_P))
+#define N_GO_P (N_GO)
+#define N_ALL_P ((N_GR_P) + (N_GO_P))
+
+#define T_GR_P (N_GR_P)
+#define T_GO_P ((T_GR_P) + (N_GO_P))
